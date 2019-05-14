10c762c957c4a9e8514336bbdc47edec47ce5a6d
Michael Redig <mredig@gmail.com>
2019-05-14 11:07:03 -0500

    initial commit

---
+++ ColorPicker/ColorWheel.swift
@@ -0,0 +1,21 @@
+//
+//  ColorWheel.swift
+//  ColorPicker
+//
+//  Created by Michael Redig on 5/14/19.
+//  Copyright © 2019 Red_Egg Productions. All rights reserved.
+//
+
+import UIKit
+
+class ColorWheel: UIControl {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ ColorPicker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColorPicker
+//
+//  Created by Michael Redig on 5/14/19.
+//  Copyright © 2019 Red_Egg Productions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
